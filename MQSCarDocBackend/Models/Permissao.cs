621b2d9e70c48a1ec1b47dc2e08e0f4df7e767db
Marcelo Marques <marcelomarqueslima.dev@outlook.com>
2025-03-25 15:27:32 -0300

    Iniciando a criação de permissões e acessos

---
+++ MQSCarDocBackend/Models/Permissao.cs
@@ -0,0 +1,17 @@
+﻿namespace MQSGestaoBackend.Models
+{
+    public class Permissao
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public Usuario NomeUsuario { get; set; }
+        public bool Leitura { get; set; }
+        public bool Escrita { get; set; }
+        public bool Exclusao { get; set; }
+
+        public Permissao()
+        {
+            Codigo = "Per00" + Id;
+        }
+    }
+}
