0430efe08a643e675c7ed074860125ab78eaddbe
Marcelo Marques <marcelomarqueslima.dev@outlook.com>
2025-03-26 07:36:43 -0300

    Teste

---
+++ MQSCarDocFrontend/Context/MqsCarDocContext.cs
@@ -0,0 +1,17 @@
+﻿using MQSMQSCarDoc.Models;
+
+namespace MQSCarDocFrontend.Context
+{
+    /*public class MqsCarDocContext : DbContext
+    {
+        public MqsCarDocContext() : base("name=ConnectionString") { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<CadTipProcesso> CadTipProcesso { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+        public DbSet<Marca> Marca { get; set; }
+        public DbSet<Modelo> Modelo { get; set; }
+        public DbSet<Permissao> Permissao { get; set; }
+    }*/
+}
